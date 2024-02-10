a9118b20675c60daed6e510a4653a57f8fa459a5
Shulin Zhang <zhangshulin@go.byuh.edu>
2024-02-09 19:08:59 -0700

    Add project files.

---
+++ Mission#5 Hobbies Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission_5_Hobbies_Web.Models;
+using System.Diagnostics;
+
+namespace Mission_5_Hobbies_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            Calculator model = new Calculator();
+            // Initialize model properties if necessary
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission#5 Hobbies Web/Models/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Mission_5_Hobbies_Web.Models
+{
+    public class Calculator
+    {
+        public int? Hours { get; set; }
+
+        public decimal HourlyRate { get; set; } = 50; // Default hourly rate
+
+    }
+}
